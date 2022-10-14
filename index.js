6b9376956b32e423b6ec915a6622cb99fb3143e0
Zehedina Smajlovic <usatransport16@gmail.com>
2022-10-13 20:51:10 -0600

    commit tree c422fd8fa1333ec057c98ef0895315ec54f979d8

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1=31
+const num2=2
+const multiply= num1* num2
 
-// Write your code here
+const random = Math.ceil (Math.random()*10);
+let num3 = 46;
+let num4 = 6;
+let mod =num3 % num4;
+const max = Math.max (4,8,12,20)
+    
